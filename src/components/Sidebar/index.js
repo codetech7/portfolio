2c8af37f783ef87b0cbe856a84a8fef12aef02d1
codetech7 <codetechseven@gmail.com>
2022-07-14 21:38:46 +0100

    Made changes to Sidebar image

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 import {Link, NavLink} from 'react-router-dom';
 import LogoS from '../../assets/images/logo-y.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoSubtitle from '../../assets/images/logo-sub2.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub, faTwitter, faDiscord } from "@fortawesome/free-brands-svg-icons";
