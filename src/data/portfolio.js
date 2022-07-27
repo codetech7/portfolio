d6430aa04dbd217cc88cf78c7350a61288f70b60
codetech7 <codetechseven@gmail.com>
2022-07-27 03:42:59 +0100

    Added New Images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,31 +2,31 @@
 module.exports = {
     "portfolio" : [
         {
-            "cover": require("../assets/images/portfolioImages/two.jpg"),
+            "cover": require("../assets/images/portfolioImages/BuyCoffee.JPG"),
             "title": "Ethereum based charity donations tracking solution",
             "url": "https://africharitydao.herokuapp.com",
             "description": "Solidity, ReactJS, Web3JS, Next, Heroku, Git"
         },
         {
-            "cover": require("../assets/images/portfolioImages/two.jpg"),
+            "cover": require("../assets/images/portfolioImages/one.jpg"),
             "title": "Ethereum based charity donations tracking solution",
             "url": "https://africharitydao.herokuapp.com",
             "description": "Solidity, ReactJS, Web3JS, Next, Heroku, Git"
         },
         {
-            "cover": require("../assets/images/portfolioImages/two.jpg"),
+            "cover": require("../assets/images/portfolioImages/EthereumLottery.JPG"),
             "title": "Ethereum based charity donations tracking solution",
             "url": "https://africharitydao.herokuapp.com",
             "description": "Solidity, ReactJS, Web3JS, Next, Heroku, Git"
         },
         {
-            "cover": require("../assets/images/portfolioImages/two.jpg"),
+            "cover": require("../assets/images/portfolioImages/Multisigwallet.JPG"),
             "title": "Ethereum based charity donations tracking solution",
             "url": "https://africharitydao.herokuapp.com",
             "description": "Solidity, ReactJS, Web3JS, Next, Heroku, Git"
         },
         {
-            "cover": require("../assets/images/portfolioImages/two.jpg"),
+            "cover": require("../assets/images/portfolioImages/MyPortfolioSite.JPG"),
             "title": "Ethereum based charity donations tracking solution",
             "url": "https://africharitydao.herokuapp.com",
             "description": "Solidity, ReactJS, Web3JS, Next, Heroku, Git"
